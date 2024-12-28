997d13cf6b4b4b1d7a805579d07e5b30c25f484f
Radislav <1z2x3c4v5b6n7m8z@gmail.com>
2024-12-28 17:00:26 +0300

    3 - Auth handled and routes in client

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,11 +3,24 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
+// Q.6 QueryClient, state management tools, redux
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+// Q options for queryClient
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false
+    }
+  }
+});
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
     </BrowserRouter>
   </StrictMode>
 );
